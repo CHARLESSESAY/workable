c112601b1248ae57e85b090b15d3e5df7695963b
CHARLESSESAY <137960739+CHARLESSESAY@users.noreply.github.com>
2026-06-24 21:01:43 +0000

    Update localStorage.js

--- firebase/firebase/services/services/features/utils/utils/utils/utils/utils/localStorage.js
+++ firebase/firebase/services/services/features/utils/utils/utils/utils/utils/localStorage.js
@@ -1,7 +1,6 @@
 export function saveState(state) {
   try { localStorage.setItem('wp_state', JSON.stringify(state)); } catch {}
 }
-
 export function loadState() {
   try {
     const saved = localStorage.getItem('wp_state');
